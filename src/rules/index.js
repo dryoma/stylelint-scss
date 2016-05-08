c6887898c2dcd1a37bd2edf14eafe9549ab76e18
dryoma <dryoma@users.noreply.github.com>
2016-05-08 21:55:56 +0800

    Add the interface script for a stylelint plugin
    And update stylelint dependency to be able the "one plugin - multiple rules"
    interface

---
+++ src/rules/index.js
@@ -0,0 +1,13 @@
+import atFunctionPattern from "./at-function-pattern"
+import atMixinPattern from "./at-mixin-pattern"
+import dollarVariablePattern from "./dollar-variable-pattern"
+import extendRequirePlaceholder from "./extend-require-placeholder"
+import selectorNoRedundantParent from "./selector-no-redundant-parent"
+
+export default {
+  "at-function-pattern": atFunctionPattern,
+  "at-mixin-pattern": atMixinPattern,
+  "dollar-variable-pattern": dollarVariablePattern,
+  "extend-require-placeholder": extendRequirePlaceholder,
+  "selector-no-redundant-parent": selectorNoRedundantParent,
+}
